56af9cec894a85f4b8f59626221ccc284ed70f8a
Your Name <your-email@example.com>
2025-12-28 22:10:39 +0300

    fix: Set basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -5,10 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/dogymorbios' : '',
+  basePath: '/dogymorbios',
   trailingSlash: true,
 }
 
 module.exports = nextConfig
-
-
